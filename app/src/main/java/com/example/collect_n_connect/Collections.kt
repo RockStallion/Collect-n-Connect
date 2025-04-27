aefb51a98630e27696b44cfe39a858db63847dc5
cesar <cesaryael.j.l@gmail.com>
2025-04-27 17:21:06 -0600

    Movimiento entre paginas

--- app/src/main/java/com/example/collect_n_connect/Collections.kt
+++ app/src/main/java/com/example/collect_n_connect/Collections.kt
@@ -1,11 +1,41 @@
 package com.example.collect_n_connect
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 
 class Collections : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.collections) // Asegúrate que este layout exista
+        setContentView(R.layout.collections)
+        val PagScan: LinearLayout = findViewById(R.id.Scan)
+        val PagSets: LinearLayout = findViewById(R.id.Sets)
+        val PagPieces: LinearLayout = findViewById(R.id.Pieces)
+        val PagYou: LinearLayout = findViewById(R.id.you)
+
+
+        PagScan.setOnClickListener {
+            val intent = Intent(this, Scan::class.java)
+            startActivity(intent)
+        }
+
+        PagSets.setOnClickListener {
+            val intent = Intent(this, Sets::class.java)
+            startActivity(intent)
+        }
+
+        PagPieces.setOnClickListener {
+            val intent = Intent(this, Pieces::class.java)
+            startActivity(intent)
+        }
+
+        PagYou.setOnClickListener {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/collect_n_connect/Pieces.kt
+++ app/src/main/java/com/example/collect_n_connect/Pieces.kt
@@ -1,11 +1,42 @@
 package com.example.collect_n_connect
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class Pieces : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pieces) // Asegúrate que este layout exista
+        setContentView(R.layout.pieces)
+
+        val PagScan: LinearLayout = findViewById(R.id.Scan)
+        val PagSets: LinearLayout = findViewById(R.id.Sets)
+        val PagPieces: LinearLayout = findViewById(R.id.Pieces)
+        val PagYou: LinearLayout = findViewById(R.id.you)
+
+
+        PagScan.setOnClickListener {
+            val intent = Intent(this, com.example.collect_n_connect.Scan::class.java)
+            startActivity(intent)
+        }
+
+        PagSets.setOnClickListener {
+            val intent = Intent(this, Sets::class.java)
+            startActivity(intent)
+        }
+
+        PagPieces.setOnClickListener {
+            val intent = Intent(this, com.example.collect_n_connect.Pieces::class.java)
+            startActivity(intent)
+        }
+
+        PagYou.setOnClickListener {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
     }
 }
